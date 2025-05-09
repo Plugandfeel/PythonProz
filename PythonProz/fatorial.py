c3e75a57d4fa5eac35b5f1b025563f33b8f5d0bf
Leo Brito <98231219+Plugandfeel@users.noreply.github.com>
2025-05-09 20:03:19 -0300

    Update fatorial.py

--- PythonProz/fatorial.py
+++ PythonProz/fatorial.py
@@ -2,7 +2,7 @@ numero = int (input ("Digite um numero"))
 if numero == 0 or numero == 1:
     print ("Fatorial igual a 1.")    
 multiplicacao = numero
-soma = 0
+
 while numero > 1:
     multiplicacao = multiplicacao * (numero-1)
     numero = numero - 1
